63a3737964b6a9dfb5e54e65dcd36f4b76afec03
Vladimir Sokolov <znoika@users.noreply.github.com>
2019-01-28 14:35:18 +0100

    added stack implementation

--- script/main.js
+++ script/main.js
@@ -1,9 +1,40 @@
+var stack = {
+
+    counter : 0,
+    top     : 0,
+    items   : [],
+
+    /**
+     * @return {string} 
+     */
+    push : function(item) {
+        this.items[this.counter] = item;
+        this.top = this.counter;
+        this.counter += 1;
+    },
+
+    pull : function() {
+        if (this.top == 0) {
+            return this.items[this.top];
+        } else {
+            var temp = this.top;
+            this.top -= 1;
+            return this.items[temp];
+        }
+    }
+
+}
+
+console.log(stack.items);
+
+stack.push("{");
+stack.push("(");
+stack.push(")");
 
 
-
-var a = 1;
-var b = 2;
-var c = 2;
+var a = stack.pull();
+var b = stack.pull();
+var c = stack.pull();
 var result = false;
 
 document.getElementById("a").innerHTML = "Katet a=" + a;
@@ -11,21 +42,4 @@ document.getElementById("b").innerHTML = "Katet b=" + b;
 document.getElementById("c").innerHTML = "Katet c=" + c;
 document.getElementById("d").innerHTML = "";
 
-var Stack = {
 
-    counter : 0,
-    items : [],
-
-    /**
-     * @return {string} 
-     */
-    set push(item) {
-        this.items[counter] = item;
-        this.counter += 1;
-    },
-
-    get pull() {
-        
-    }
-
-}
